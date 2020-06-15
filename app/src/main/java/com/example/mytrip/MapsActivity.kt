88578b8af19c1d3340cacd3772a5846d4b65f748
Caiango <cvff12345@gmail.com>
2020-06-15 00:49:00 -0300

    Implementamdo localização atual do usuário

--- app/src/main/java/com/example/mytrip/MapsActivity.kt
+++ app/src/main/java/com/example/mytrip/MapsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.mytrip
 
+import android.content.Intent
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,20 +12,62 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import mumayank.com.airlocationlibrary.AirLocation
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    var airloc: AirLocation? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        if (mMap!= null){
+
+            airloc = AirLocation(this, true, true,
+                object : AirLocation.Callbacks{
+                    override fun onFailed(locationFailedEnum: AirLocation.LocationFailedEnum) {
+
+                        Toast.makeText(applicationContext, "ERRO", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onSuccess(location: Location) {
+
+                        val ll = LatLng(location.latitude, location.longitude)
+                        mMap.addMarker(MarkerOptions().position(ll).title("Marker in where you are"))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll,16.0f))
+                    }
+
+                })
+        }
+
+        // Add a marker in Sydney and move the camera
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        airloc?.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        airloc?.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -32,12 +77,5 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
 }
